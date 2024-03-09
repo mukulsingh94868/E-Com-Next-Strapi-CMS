db6f2617389b7778d8b1faad3a17e495f34da39c
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-03-09 23:14:18 +0530

    after install dependencies

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-    return <div className="text">Header</div>;
+    return <div className="text">Header Header Header</div>;
 };
 
 export default Header;
